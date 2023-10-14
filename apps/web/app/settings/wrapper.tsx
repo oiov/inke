493b33eb2216d394537d817a42881a7d472ffbb2
yesmore <3224266014@qq.com>
2023-10-14 16:22:01 +0800

    add setting tips

--- apps/web/app/settings/wrapper.tsx
+++ apps/web/app/settings/wrapper.tsx
@@ -1,12 +1,22 @@
 "use client";
 
 import { Session } from "next-auth";
+import Image from "next/image";
 
 export default function Wrapper({ session }: { session: Session | null }) {
   return (
     <>
       <div className="mx-auto h-screen max-w-3xl px-6">
-        <h1></h1>
+        <div className="flex flex-col items-center justify-center">
+          <Image
+            src="/cat.png"
+            alt="404"
+            width="250"
+            height="250"
+            className="ml-4 rounded-sm"
+          />
+          <p>Please waiting...</p>
+        </div>
       </div>
     </>
   );
