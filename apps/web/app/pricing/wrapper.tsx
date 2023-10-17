de82156e367b854c2f11384c6f2332946765e5d3
songjunxi <3224266014@qq.com>
2023-10-17 10:21:33 +0800

    update plan btn text

--- apps/web/app/pricing/wrapper.tsx
+++ apps/web/app/pricing/wrapper.tsx
@@ -74,7 +74,7 @@ export function PlanCards({ activeIndex }: { activeIndex: string }) {
             </div>
             <div className="mt-6">
               <button className="w-full rounded-lg bg-black px-3 py-2 font-semibold text-slate-100 shadow-md">
-                Beta for free
+                Get start
               </button>
             </div>
           </div>
@@ -126,7 +126,7 @@ export function PlanCards({ activeIndex }: { activeIndex: string }) {
             </div>
             <div className="mt-6">
               <button className="w-full rounded-lg bg-black px-3 py-2 font-semibold text-slate-100 shadow-md">
-                Beta for free
+                Sign in for free
               </button>
             </div>
           </div>
