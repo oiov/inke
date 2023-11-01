7c7e8b41cdf61d97b58c9b161acdcda33bd3b1a5
songjunxi <3224266014@qq.com>
2023-11-01 17:37:28 +0800

    add try catch

--- apps/web/app/api/generate/bot/route.ts
+++ apps/web/app/api/generate/bot/route.ts
@@ -16,85 +16,94 @@ const openai = new OpenAI({
 export const runtime = "edge";
 
 export async function POST(req: Request): Promise<Response> {
-  // Check if the OPENAI_API_KEY is set, if not return 400
-  if (!process.env.OPENAI_API_KEY || process.env.OPENAI_API_KEY === "") {
-    return new Response(
-      "Missing OPENAI_API_KEY – make sure to add it to your .env file.",
-      {
-        status: 400,
-      },
-    );
-  }
-
-  const { prompt, plan, messages, system } = await req.json();
-
-  const planN = Number(plan || "5");
-
-  if (
-    messages &&
-    messages.length > Account_Plans[planN].ai_bot_history_length
-  ) {
-    return new Response("You have reached the history message limit.", {
-      status: 429,
-    });
-  }
-
-  if (process.env.KV_REST_API_URL && process.env.KV_REST_API_TOKEN) {
-    const ip = req.headers.get("x-forwarded-for");
-    const ratelimit = new Ratelimit({
-      redis: kv,
-      limiter: Ratelimit.slidingWindow(
-        Account_Plans[planN].ai_generate_day,
-        "1 d",
-      ),
-    });
-
-    // console.log("plan", planN, Account_Plans[planN], ip);
-
-    const { success, limit, reset, remaining } = await ratelimit.limit(
-      `novel_ratelimit_${ip}`,
-    );
-
-    if (!success) {
-      return new Response("You have reached your request limit for the day.", {
-        status: 429,
-        headers: {
-          "X-RateLimit-Limit": limit.toString(),
-          "X-RateLimit-Remaining": remaining.toString(),
-          "X-RateLimit-Reset": reset.toString(),
+  try {
+    // Check if the OPENAI_API_KEY is set, if not return 400
+    if (!process.env.OPENAI_API_KEY || process.env.OPENAI_API_KEY === "") {
+      return new Response(
+        "Missing OPENAI_API_KEY – make sure to add it to your .env file.",
+        {
+          status: 400,
         },
+      );
+    }
+
+    const { prompt, plan, messages, system } = await req.json();
+
+    const planN = Number(plan || "5");
+
+    if (
+      messages &&
+      messages.length > Account_Plans[planN].ai_bot_history_length
+    ) {
+      return new Response("You have reached the history message limit.", {
+        status: 429,
       });
     }
+
+    if (process.env.KV_REST_API_URL && process.env.KV_REST_API_TOKEN) {
+      const ip = req.headers.get("x-forwarded-for");
+      const ratelimit = new Ratelimit({
+        redis: kv,
+        limiter: Ratelimit.slidingWindow(
+          Account_Plans[planN].ai_generate_day,
+          "1 d",
+        ),
+      });
+
+      // console.log("plan", planN, Account_Plans[planN], ip);
+
+      const { success, limit, reset, remaining } = await ratelimit.limit(
+        `novel_ratelimit_${ip}`,
+      );
+
+      if (!success) {
+        return new Response(
+          "You have reached your request limit for the day.",
+          {
+            status: 429,
+            headers: {
+              "X-RateLimit-Limit": limit.toString(),
+              "X-RateLimit-Remaining": remaining.toString(),
+              "X-RateLimit-Reset": reset.toString(),
+            },
+          },
+        );
+      }
+    }
+
+    openai.apiKey = getRandomElement(api_keys.split(",")) || api_key;
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo-16k",
+      messages: [
+        {
+          role: "system",
+          content:
+            "As a note assistant, communicate with users based on the input note content." +
+            "Do not reply to questions unrelated to the notes. If there are questions unrelated to the notes, please reply 'Please ask questions related to the notes'",
+        },
+        {
+          role: "system",
+          content: `Note content: \n${system}`,
+        },
+        ...messages,
+      ],
+      temperature: 0.7,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      stream: true,
+      n: 1,
+    });
+
+    // Convert the response into a friendly text-stream
+    const stream = OpenAIStream(response);
+
+    // Respond with the stream
+    return new StreamingTextResponse(stream);
+  } catch (error) {
+    return new Response("Server error", {
+      status: 500,
+    });
   }
-
-  openai.apiKey = getRandomElement(api_keys.split(",")) || api_key;
-
-  const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo-16k",
-    messages: [
-      {
-        role: "system",
-        content:
-          "As a note assistant, communicate with users based on the input note content." +
-          "Do not reply to questions unrelated to the notes. If there are questions unrelated to the notes, please reply 'Please ask questions related to the notes'",
-      },
-      {
-        role: "system",
-        content: `Note content: \n${system}`,
-      },
-      ...messages,
-    ],
-    temperature: 0.7,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-    stream: true,
-    n: 1,
-  });
-
-  // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response);
-
-  // Respond with the stream
-  return new StreamingTextResponse(stream);
 }
--- apps/web/app/api/generate/edit/route.ts
+++ apps/web/app/api/generate/edit/route.ts
@@ -14,79 +14,88 @@ const openai = new OpenAI();
 export const runtime = "edge";
 
 export async function POST(req: Request): Promise<Response> {
-  // Check if the OPENAI_API_KEY is set, if not return 400
-  if (!process.env.OPENAI_API_KEY || process.env.OPENAI_API_KEY === "") {
-    return new Response(
-      "Missing OPENAI_API_KEY – make sure to add it to your .env file.",
-      {
-        status: 400,
-      },
-    );
-  }
-
-  const { prompt, plan } = await req.json();
-
-  const planN = Number(plan || "5");
-
-  if (process.env.KV_REST_API_URL && process.env.KV_REST_API_TOKEN) {
-    const ip = req.headers.get("x-forwarded-for");
-    const ratelimit = new Ratelimit({
-      redis: kv,
-      limiter: Ratelimit.slidingWindow(
-        Account_Plans[planN].ai_generate_day,
-        "1 d",
-      ),
-    });
-
-    // console.log("plan", planN, Account_Plans[planN], ip);
-
-    const { success, limit, reset, remaining } = await ratelimit.limit(
-      `novel_ratelimit_${ip}`,
-    );
-
-    if (!success) {
-      return new Response("You have reached your request limit for the day.", {
-        status: 429,
-        headers: {
-          "X-RateLimit-Limit": limit.toString(),
-          "X-RateLimit-Remaining": remaining.toString(),
-          "X-RateLimit-Reset": reset.toString(),
+  try {
+    // Check if the OPENAI_API_KEY is set, if not return 400
+    if (!process.env.OPENAI_API_KEY || process.env.OPENAI_API_KEY === "") {
+      return new Response(
+        "Missing OPENAI_API_KEY – make sure to add it to your .env file.",
+        {
+          status: 400,
         },
-      });
+      );
     }
-  }
 
-  openai.apiKey = getRandomElement(api_keys.split(",")) || api_key;
+    const { prompt, plan } = await req.json();
 
-  const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo-16k",
-    messages: [
-      {
-        role: "system",
-        content: `I hope you can take on roles such as spell proofreading and rhetorical improvement, 
+    const planN = Number(plan || "5");
+
+    if (process.env.KV_REST_API_URL && process.env.KV_REST_API_TOKEN) {
+      const ip = req.headers.get("x-forwarded-for");
+      const ratelimit = new Ratelimit({
+        redis: kv,
+        limiter: Ratelimit.slidingWindow(
+          Account_Plans[planN].ai_generate_day,
+          "1 d",
+        ),
+      });
+
+      // console.log("plan", planN, Account_Plans[planN], ip);
+
+      const { success, limit, reset, remaining } = await ratelimit.limit(
+        `novel_ratelimit_${ip}`,
+      );
+
+      if (!success) {
+        return new Response(
+          "You have reached your request limit for the day.",
+          {
+            status: 429,
+            headers: {
+              "X-RateLimit-Limit": limit.toString(),
+              "X-RateLimit-Remaining": remaining.toString(),
+              "X-RateLimit-Reset": reset.toString(),
+            },
+          },
+        );
+      }
+    }
+
+    openai.apiKey = getRandomElement(api_keys.split(",")) || api_key;
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo-16k",
+      messages: [
+        {
+          role: "system",
+          content: `I hope you can take on roles such as spell proofreading and rhetorical improvement, 
            or other roles related to text editing, optimization, and abbreviation. I will 
            communicate with you in any language, and you will recognize the language. Please only answer the corrected and improved parts, and 
            do not write explanations.
            Limit your response to no more than ${Account_Plans[planN].ai_generate_chars} characters, 
            but make sure to construct complete sentences.`,
-        // "Use Markdown formatting when appropriate.",
-      },
-      {
-        role: "user",
-        content: prompt,
-      },
-    ],
-    temperature: 0.7,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-    stream: true,
-    n: 1,
-  });
+          // "Use Markdown formatting when appropriate.",
+        },
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+      temperature: 0.7,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      stream: true,
+      n: 1,
+    });
 
-  // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response);
+    // Convert the response into a friendly text-stream
+    const stream = OpenAIStream(response);
 
-  // Respond with the stream
-  return new StreamingTextResponse(stream);
+    // Respond with the stream
+    return new StreamingTextResponse(stream);
+  } catch (error) {
+    return new Response("Server error", {
+      status: 500,
+    });
+  }
 }
