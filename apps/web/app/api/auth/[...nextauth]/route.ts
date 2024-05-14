d53dc285dd4c850a4d5a5e6bb3a6757617fd0ae7
yesmore <oiov@duck.com>
2024-05-14 15:21:52 +0800

    Update route.ts

--- apps/web/app/api/auth/[...nextauth]/route.ts
+++ apps/web/app/api/auth/[...nextauth]/route.ts
@@ -9,10 +9,10 @@ import { createTransport } from "nodemailer";
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-    }),
+    // GithubProvider({
+    //   clientId: process.env.GITHUB_ID as string,
+    //   clientSecret: process.env.GITHUB_SECRET as string,
+    // }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
